65efba2b71cd2d8cdce03a3fcb43c6a59c293e16
Tambua Evaristus <evaristustambua@gmail.com>
2022-10-01 06:44:33 +0100

    text shadow working

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -28,10 +28,10 @@ function Banner({ netflixOriginals }: Props) {
       </div>
 
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">{movie?.title || movie?.name || movie?.original_name}</h1>
-      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
 
-      <div className="flex"> 
-        <button className="bannerButton  bg-white text-black">Play <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /></button>
+      <div className="flex space-x-3"> 
+        <button className="bannerButton bg-white text-black">Play <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /></button>
         <button className="bannerButton bg-[gray]/70">More Info <FaInfoCircle className="h-5 w-5 text-black md:h-8 md:w-8"/></button>
       </div>
     </div>
